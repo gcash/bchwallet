btcsuite/btcwallet

wtxmgr+wallet: add wtxmgr sublogger

wallet/log.go
@@ -8,6 +8,7 @@ import (
 	"github.com/btcsuite/btclog"
 	"github.com/btcsuite/btcwallet/waddrmgr"
 	"github.com/btcsuite/btcwallet/walletdb/migration"
+	"github.com/btcsuite/btcwallet/wtxmgr"
 )
 
 // log is a logger that is initialized with no output filters.  This
@@ -34,6 +35,7 @@ func UseLogger(logger btclog.Logger) {
 
 	migration.UseLogger(logger)
 	waddrmgr.UseLogger(logger)
+	wtxmgr.UseLogger(logger)
 }
 
 // LogClosure is a closure that can be printed with %v to be used to

wtxmgr/log.go
@@ -9,12 +9,17 @@ import "github.com/btcsuite/btclog"
 // log is a logger that is initialized with no output filters.  This
 // means the package will not perform any logging by default until the caller
 // requests it.
-var log = btclog.Disabled
+var log btclog.Logger
+
+// The default amount of logging is none.
+func init() {
+	DisableLog()
+}
 
 // DisableLog disables all library log output.  Logging output is disabled
 // by default until either UseLogger or SetLogWriter are called.
 func DisableLog() {
-	log = btclog.Disabled
+	UseLogger(btclog.Disabled)
 }
 
 // UseLogger uses a specified Logger to output package logging info.
@@ -22,4 +27,21 @@ func DisableLog() {
 // using btclog.
 func UseLogger(logger btclog.Logger) {
 	log = logger
+}
+
+// LogClosure is a closure that can be printed with %v to be used to
+// generate expensive-to-create data for a detailed log level and avoid doing
+// the work if the data isn't printed.
+type logClosure func() string
+
+// String invokes the log closure and returns the results string.
+func (c logClosure) String() string {
+	return c()
+}
+
+// newLogClosure returns a new closure over the passed function which allows
+// it to be used as a parameter in a logging function that is only invoked when
+// the logging level is such that the message will actually be logged.
+func newLogClosure(c func() string) logClosure {
+	return logClosure(c)
 }

gcash/bchwallet

wallet/log.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gcash/bchlog"
 	"github.com/gcash/bchwallet/waddrmgr"
 	"github.com/gcash/bchwallet/walletdb/migration"
+	"github.com/gcash/bchwallet/wtxmgr"
 )
 
 // log is a logger that is initialized with no output filters.  This
@@ -34,6 +35,7 @@ func UseLogger(logger bchlog.Logger) {
 
 	migration.UseLogger(logger)
 	waddrmgr.UseLogger(logger)
+	wtxmgr.UseLogger(logger)
 }
 
 // LogClosure is a closure that can be printed with %v to be used to

wtxmgr/log.go
@@ -11,12 +11,17 @@ import (
 // log is a logger that is initialized with no output filters.  This
 // means the package will not perform any logging by default until the caller
 // requests it.
-var log = bchlog.Disabled
+var log bchlog.Logger
+
+// The default amount of logging is none.
+func init() {
+	DisableLog()
+}
 
 // DisableLog disables all library log output.  Logging output is disabled
 // by default until either UseLogger or SetLogWriter are called.
 func DisableLog() {
-	log = bchlog.Disabled
+	UseLogger(bchlog.Disabled)
 }
 
 // UseLogger uses a specified Logger to output package logging info.
@@ -24,4 +29,21 @@ func DisableLog() {
 // using bchlog.
 func UseLogger(logger bchlog.Logger) {
 	log = logger
+}
+
+// LogClosure is a closure that can be printed with %v to be used to
+// generate expensive-to-create data for a detailed log level and avoid doing
+// the work if the data isn't printed.
+type logClosure func() string
+
+// String invokes the log closure and returns the results string.
+func (c logClosure) String() string {
+	return c()
+}
+
+// newLogClosure returns a new closure over the passed function which allows
+// it to be used as a parameter in a logging function that is only invoked when
+// the logging level is such that the message will actually be logged.
+func newLogClosure(c func() string) logClosure {
+	return logClosure(c)
 }