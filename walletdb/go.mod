btcsuite/btcwallet

build+walletdb: create new module for walletdb
In this commit, we create a new module for `walletdb` in order to allow
external projects to use the project without also having to depend on
the entirety of `btcwallet`. With this commit, projects that depend on
`walletdb` will benefit from a slimmer set of additional dependencies.

walletdb/go.mod
@@ -1,0 +1,11 @@
+module github.com/btcsuite/btcwallet/walletdb
+
+go 1.12
+
+require (
+	github.com/btcsuite/btclog v0.0.0-20170628155309-84c8d2346e9f
+	github.com/coreos/bbolt v1.3.3
+	github.com/davecgh/go-spew v1.1.1
+	go.etcd.io/bbolt v1.3.3 // indirect
+	golang.org/x/sys v0.0.0-20190904154756-749cb33beabd // indirect
+)

gcash/bchwallet

walletdb/go.mod
@@ -1,0 +1,11 @@
+module github.com/gcash/bchwallet/walletdb
+
+go 1.12
+
+require (
+	github.com/bchsuite/bchlog v0.0.0-20170628155309-84c8d2346e9f
+	github.com/coreos/bbolt v1.3.3
+	github.com/davecgh/go-spew v1.1.1
+	go.etcd.io/bbolt v1.3.3 // indirect
+	golang.org/x/sys v0.0.0-20190904154756-749cb33beabd // indirect
+)